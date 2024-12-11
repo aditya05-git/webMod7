0c05586cd1da646b96f37686ddb23137fa2ca00d
aditya05-git <20230810070@uniku.ac.id>
2024-12-11 12:57:11 +0700

    Add files via upload

---
+++ modul4&5/js/jsM43.js
@@ -0,0 +1,10 @@
+function pesan(){
+    alert("Ini Latihan Javascript Pertama Saya!");
+}
+function pesanConsole(){
+    console.log("it's Work");
+}
+function pesanDoc(){
+    document.write("Succes <br>");
+    document.write("<a href='m4-2.html'>Kembali</a>");
+}
